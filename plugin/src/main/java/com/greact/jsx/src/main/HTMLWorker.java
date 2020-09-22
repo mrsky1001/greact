9f374e4279d6bcfc677f4853e64e8fc6304b8b79
mrsky1001 <nikita.nk16@yandex.ru>
2020-09-22 18:00:50 +0600

    try get tag text

--- plugin/src/main/java/com/greact/jsx/src/main/HTMLWorker.java
+++ plugin/src/main/java/com/greact/jsx/src/main/HTMLWorker.java
@@ -9,78 +9,113 @@ import java.util.Iterator;
 import java.util.List;
 
 public class HTMLWorker extends HTMLParserBaseListener {
-
-    public void enterHtmlDocument(HTMLParser.HtmlDocumentContext ctx) {
-        Iterator iterator = ctx.children.iterator();
-
+    private void recurse(Iterator iterator) {
         while (iterator.hasNext()) {
             Object elem = iterator.next();
+
             try {
                 System.out.println(((TerminalNodeImpl) elem).getText());
-            } catch (Exception e) {
-
+            } catch (Exception ignored) {
             }
 
             try {
                 System.out.println(((HtmlTagNameContext) elem).getText());
-            } catch (Exception e) {
-
+            } catch (Exception ignored) {
             }
 
             try {
                 System.out.println(((HTMLParser.HtmlAttributeContext) elem).getText());
-            } catch (Exception e) {
-
+            } catch (Exception ignored) {
             }
 
-            Iterator iterator2 = ((HTMLParser.HtmlElementsContext) elem).children.iterator();
-            while (iterator2.hasNext()) {
-                Object elem2 = iterator2.next();
+            Iterator iterator2 = null;
 
-                try {
-                    System.out.println(((TerminalNodeImpl) elem2).getText());
-                } catch (Exception e) {
-
-                }
-
-                try {
-                    System.out.println(((HtmlTagNameContext) elem2).getText());
-                } catch (Exception e) {
-
-                }
-
-                try {
-                    System.out.println(((HTMLParser.HtmlAttributeContext) elem2).getText());
-                } catch (Exception e) {
-
-                }
-
-                Iterator iterator3 = ((HTMLParser.HtmlElementContext) elem2).children.iterator();
-                while (iterator3.hasNext()) {
-                    Object elem3 = iterator3.next();
-                    try {
-                        System.out.println(((TerminalNodeImpl) elem3).getText());
-                    } catch (Exception e) {
-
-                    }
-
-                    try {
-                        System.out.println(((HtmlTagNameContext) elem3).getText());
-                    } catch (Exception e) {
-
-                    }
-
-                    try {
-                        System.out.println(((HTMLParser.HtmlAttributeContext) elem3).getText());
-                    } catch (Exception e) {
-
-                    }
-//                    System.out.println(( (HTMLParser.HtmlElementContext) elem3).getStart().getText());
-                }
+            try {
+                iterator2 = ((HTMLParser.HtmlElementsContext) elem).children.iterator();
+                recurse(iterator2);
+            } catch (Exception ignored) {
             }
 
-            System.out.println(elem);
+            if (iterator2 == null)
+                try {
+                    iterator2 = ((HTMLParser.HtmlElementContext) elem).children.iterator();
+                    recurse(iterator2);
+                } catch (Exception ignored) {
+                }
         }
+    }
+
+    public void enterHtmlDocument(HTMLParser.HtmlDocumentContext ctx) {
+        Iterator iterator = ctx.children.iterator();
+        recurse(iterator);
+//        while (iterator.hasNext()) {
+//            Object elem = iterator.next();
+//            try {
+//                System.out.println(((TerminalNodeImpl) elem).getText());
+//            } catch (Exception e) {
+//
+//            }
+//
+//            try {
+//                System.out.println(((HtmlTagNameContext) elem).getText());
+//            } catch (Exception e) {
+//
+//            }
+//
+//            try {
+//                System.out.println(((HTMLParser.HtmlAttributeContext) elem).getText());
+//            } catch (Exception e) {
+//
+//            }
+//
+//            Iterator iterator2 = ((HTMLParser.HtmlElementsContext) elem).children.iterator();
+//            while (iterator2.hasNext()) {
+//                Object elem2 = iterator2.next();
+//
+//                try {
+//                    System.out.println(((TerminalNodeImpl) elem2).getText());
+//                } catch (Exception e) {
+//
+//                }
+//
+//                try {
+//                    System.out.println(((HtmlTagNameContext) elem2).getText());
+//                } catch (Exception e) {
+//
+//                }
+//
+//                try {
+//                    System.out.println(((HTMLParser.HtmlAttributeContext) elem2).getText());
+//                } catch (Exception e) {
+//
+//                }
+//
+//                Iterator iterator3 = ((HTMLParser.HtmlElementContext) elem2).children.iterator();
+//                while (iterator3.hasNext()) {
+//                    Object elem3 = iterator3.next();
+//                    try {
+//                        System.out.println(((TerminalNodeImpl) elem3).getText());
+//                    } catch (Exception e) {
+//
+//                    }
+//
+//                    try {
+//                        System.out.println(((HtmlTagNameContext) elem3).getText());
+//                    } catch (Exception e) {
+//
+//                    }
+//
+//                    try {
+//                        System.out.println(((HTMLParser.HtmlAttributeContext) elem3).getText());
+//                    } catch (Exception e) {
+//
+//                    }
+////                    System.out.println(( (HTMLParser.HtmlElementContext) elem3).getStart().getText());
+//                }
+//            }
+//
+//            System.out.println(elem);
+//        }
 
         System.out.println(ctx);
         System.out.println("Entering: " + ctx.getText());
